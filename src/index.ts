b61d2d5307c09bd8426b14e8fdd17dee778f4695
Alexander <alexvanheteren@gmail.com>
2024-04-17 14:13:48 +0200

    exports

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,30 @@
 import "reflect-metadata";
 
-import express, { Request, Response } from 'express';
-import { AuthController } from './controllers/auth.controller';
+import express, { Request, Response } from "express";
+import { AuthController } from "./controllers/auth.controller";
 import { container } from "tsyringe";
-import { AuthService } from './services/auth.service';
-import AuthRepository from './repositories/auth.repository';
+import { AuthService } from "./services/auth.service";
+import AuthRepository from "./repositories/auth.repository";
 
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
-
 const authController = container.resolve(AuthController);
 const authService = container.resolve(AuthService);
 const authRepository = container.resolve(AuthRepository);
 
-app.post('/auth', async (req: Request, res: Response) => {
-    return await authController.auth(req, res);
-  });
+app.post("/auth", async (req: Request, res: Response) => {
+  return await authController.auth(req, res);
+});
 
-
-  app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!');
-  });
-
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-  });
+  console.log(`Server running at http://localhost:${port}`);
+});
 
-  module.exports = app;
+
+module.exports = app;
